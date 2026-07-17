8cafcbb5021ae37290083046bc82d8aa1bda973d
NguyenSonNhut <sonnhut1999@gmail.com>
2026-07-17 17:43:13 +0700

    Update Urgent Infomation

--- data/contact.ts
+++ data/contact.ts
@@ -8,16 +8,10 @@ export const contactInformation: ContactItem[] = [
     key: "email",
     value: "mannguyenartist@email.com",
   },
-  {
-    key: "phone",
-    value: "+84 98 3838 019",
-  },
+
   {
     key: "facebook",
     value: "facebook.com/mannguyenartist",
   },
-  {
-    key: "instagram",
-    value: "@mannguyen.artist",
-  },
+
 ];
